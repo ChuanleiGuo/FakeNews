277e94498e6bcde657eb0a2205185edb78f2ce37
ChuanLeiGuo <chuanleiguo@gmail.com>
2016-08-14 20:20:59 +0800

    Initial Commit

---
+++ FakeNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FakeNews
+//
+//  Created by ChuanleiGuo on 8/14/16.
+//  Copyright © 2016 ChuanleiGuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
