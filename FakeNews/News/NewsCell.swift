eea05a58da5e3d7968d2abc5e51177f97dfbca4c
ChuanLeiGuo <chuanleiguo@gmail.com>
2016-08-29 21:09:25 +0800

    Added the news entity and ui

---
+++ FakeNews/News/NewsCell.swift
@@ -0,0 +1,22 @@
+//
+//  NewsCell.swift
+//  FakeNews
+//
+//  Created by ChuanleiGuo on 8/29/16.
+//  Copyright © 2016 ChuanleiGuo. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var imgIcon: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var replyLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var imgOther1: UIImageView!
+    @IBOutlet weak var imgOther2: UIImageView!
+
+}
---
+++ FakeNews/News/NewsEntity.swift
@@ -0,0 +1,101 @@
+//
+//  NewsEntity.swift
+//  FakeNews
+//
+//  Created by ChuanleiGuo on 8/29/16.
+//  Copyright © 2016 ChuanleiGuo. All rights reserved.
+//
+
+import UIKit
+
+class NewsEntity: NSObject {
+    
+    var tname: String = ""
+    
+    /**
+     *  The publish time of the news
+     */
+    var ptime: String = ""
+    
+    /**
+     *  Title
+     */
+    var title: String = ""
+    
+    /**
+     *  The array of images
+     */
+    var imgextra: [String] = []
+    var photosetID: String = ""
+    var hasHead: NSNumber = false
+    var hasImg: NSNumber = false
+    var lmodify: String = ""
+    var template: String = ""
+    var skipType: String = ""
+    
+    /**
+     *  The number of people who followed the thread
+     */
+    var replyCount: NSNumber = 0
+    var votecount: NSNumber = 0
+    var voteCount: NSNumber = 0
+    var alias: String = ""
+    
+    /**
+     *  The ID of the news
+     */
+    var docid: String = ""
+    var hasCover: Bool = false
+    var hasAD: NSNumber = false
+    var priority: NSNumber = false
+    var cid: String = ""
+    var videoID: [String] = []
+    
+    /**
+     *  The links to pictures
+     */
+    var imgsrc: String = ""
+    var hasIcon: Bool = false
+    var ename: String = ""
+    var skipID: String = ""
+    var order: NSNumber = 0
+    
+    /**
+     *  Descriptions
+     */
+    var digest: String = ""
+    var editor: [String] = []
+    var url_3w: String = ""
+    var specialID: String = ""
+    var timeConsuming: String = ""
+    var subtitle: String = ""
+    var adTitle: String = ""
+    var url: String = ""
+    var scource: String = ""
+    
+    var TAGS: String = ""
+    var TAG: String = ""
+    
+    /**
+     *  The style of big picture
+     */
+    var boardid: String = ""
+    var commentid: String = ""
+    var speciallogo: NSNumber = 0
+    var specialtip: String = ""
+    var specialadlogo: String = ""
+    
+    var pixel: String = ""
+    var applist: [String] = []
+    
+    var wap_portal: String = ""
+    var live_info: String = ""
+    var ads: String = ""
+    var videosource: String = ""
+    
+    class func newsModel(withDict dict: [String: Any]) -> NewsEntity {
+        let model = NewsEntity()
+        model.setValuesForKeys(dict)
+        return model
+    }
+}
