0059d1ec3953482e512412b7d1ed437f952bb039
ChuanLeiGuo <chuanleiguo@gmail.com>
2016-09-04 20:10:28 +0800

    Completed the reply cell

--- FakeNews/Reply/ReplyCell.swift
+++ FakeNews/Reply/ReplyCell.swift
@@ -9,16 +9,41 @@
 import UIKit
 
 class ReplyCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    // MARK: - Properties
+    
+    var replyModel: ReplyEntity! {
+        didSet {
+            configureCell(withModel: replyModel)
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var sayLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var supposeLabel: UILabel!
+    
+    
+    // MARK: - Private Methods
+    
+    private func configureCell(withModel model: ReplyEntity) {
+        nameLabel.text = replyModel.name
+        
+        addressLabel.text = replyModel.address
+        if let rangeAddress = replyModel.address.range(of: "&nbsp") {
+            addressLabel.text = replyModel.address.substring(to: rangeAddress.lowerBound)
+        }
+        
+        sayLabel.text = replyModel.say
+        if let _ = replyModel.say.range(of: "<br>") {
+            var temp = replyModel.say
+            temp = temp.replacingOccurrences(of: "<br>", with: "\n",
+                                      options: .caseInsensitive, range: temp.startIndex..<temp.endIndex)
+            sayLabel.text = temp
+        }
+        supposeLabel.text = replyModel.suppose
+        selectionStyle = .none
     }
-
 }
