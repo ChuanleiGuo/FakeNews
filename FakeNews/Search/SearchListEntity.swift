05c750341a4083e97fdd9869187baddc2cb6f172
ChuanLeiGuo <chuanleiguo@gmail.com>
2016-09-19 09:09:35 +0800

    Implemented the search model

---
+++ FakeNews/Search/SearchListEntity.swift
@@ -0,0 +1,20 @@
+//
+//  SearchListEntity.swift
+//  FakeNews
+//
+//  Created by ChuanleiGuo on 9/19/16.
+//  Copyright © 2016 ChuanleiGuo. All rights reserved.
+//
+
+import UIKit
+
+class SearchListEntity: NSObject {
+    
+    var docid = ""
+    var title = ""
+    var skipID = ""
+    var dkeys = ""
+    var skipType = ""
+    var ptime = ""
+
+}
